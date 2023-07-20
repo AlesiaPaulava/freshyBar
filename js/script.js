8850b8a9f45dc9b8ebe1c975c279e02886196202
Alesia Paulava <sportgirl16031988@gmail.com>
2023-07-20 22:15:47 +0300

    add api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 const API_URL = 'https://polyester-daffy-authorization.glitch.me/';
+
+const getData = async () => {
+  const response = await fetch(`${API_URL}api/goods`);
+  const data = await response.json();
+  return data;
+};
+
+const createCard = (item) => {
+  const cocktail = document.createElement('article');
+  cocktail.classList.add('cocktail');
+
+  cocktail.innerHTML = `
+    <img src="${API_URL}${item.image}" alt="Коктейль ${item.title}" class="cocktail__img">
+
+    <div class="cocktail__content">
+      <div class="cocktail__text">
+        <h3 class="cocktail__title">${item.title}</h3>
+        <p class="cocktail_price text-red">${item.price} ₽</p>
+        <p class="cocktail__size">${item.size}</p>
+      </div>
+
+      <button class="btn cocktail__btn" data-id="${item.id}">Добавить</button>
+    </div>
+  `;
+  return cocktail;
+}
+
+const init = async () => {
+  const goodsListElem = document.querySelector('.goods__list');
+  const data = await getData();
+
+  const cardsCocktail = data.map((item) => {
+    const li = document.createElement('li');
+    li.classList.add('goods__item');
+    li.append(createCard(item));
+    return li;
+  });
+
+  goodsListElem.append(...cardsCocktail);
+};
+
+init();
