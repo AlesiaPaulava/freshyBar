9939f794faf055c3454acd2f1b4fb083d94e5a30
Alesia Paulava <sportgirl16031988@gmail.com>
2023-07-22 20:02:50 +0300

    add modal__make

--- js/script.js
+++ js/script.js
@@ -26,6 +26,34 @@ const createCard = (item) => {
   return cocktail;
 }
 
+const scrollService = {
+  scrollPosition: 0, 
+  disabledScroll() {
+    this.scrollPosition = window.scrollY; //определяет текущую позицию
+    document.documentElement.style.scrollBehavior = 'auto'; //чтобы при закрытии небыло скочка страницы
+    //добавляем к странице свойства для блокировки скролла
+    // top: -${this.scrollPosition}px для того,что бы страница не прыгала вверх при открытии модалки
+    //left height width - для того, чтобы страница не смещалась
+    //padding-right: ${window.innerWidth - document.body.offsetWidth}px вычесляет размер скрола и 
+    //страница не прыгает при открытии модалки
+    document.body.style.cssText = `   
+      overflow: hidden;
+      position: fixed;
+      top: -${this.scrollPosition}px;
+      left: 0;
+      height: 100vh;
+      width: 100vw;
+      padding-right: ${window.innerWidth - document.body.offsetWidth}px;
+    `;
+  },
+  enabledScroll() {
+    document.body.style.cssText = '';//удаляется всё, что было записано выше в cssText 39стр
+    window.scroll({ top: this.scrollPosition }); //проскролить страницу при закрытии модалки на место с которого была открыта модалка
+    document.documentElement.style.scrollBehavior = '';
+  }
+}
+
+//функция для открытия/закрытия модальных окон
 const modalController = ({ modal, btnOpen, time = 300 }) => {
   const buttonElem = document.querySelector(btnOpen);
   const modalElem = document.querySelector(modal);
@@ -43,9 +71,10 @@ const modalController = ({ modal, btnOpen, time = 300 }) => {
 
     if (target === modalElem || code === 'Escape') {
       modalElem.style.opacity = 0;
-
+      
       setTimeout(() => { //Для плавности закрытия окна
         modalElem.style.visibility = 'hidden';
+        scrollService.enabledScroll();
       }, time);
 
       window.removeEventListener('keydown', closeModal); //чтобы не писало в консоли нажатые кнопки
@@ -56,6 +85,7 @@ const modalController = ({ modal, btnOpen, time = 300 }) => {
     modalElem.style.visibility = 'visible';
     modalElem.style.opacity = 1;
     window.addEventListener('keydown', closeModal); //закрытие на кнопку ESC 
+    scrollService.disabledScroll();
   };
 
   buttonElem.addEventListener('click', openModal); //открыть модальное окно
@@ -69,6 +99,12 @@ const init = async () => {
     modal: '.modal__order',
     btnOpen: '.header__btn-order'
   });
+
+  modalController({ 
+    modal: '.modal__make',
+    btnOpen: '.cocktail__btn_make',
+  });
+
   const goodsListElem = document.querySelector('.goods__list');
   const data = await getData();
 
