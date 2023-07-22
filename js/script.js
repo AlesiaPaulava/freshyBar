298d0d9cfab90688ddf3ba492b76decbe5db151e
Alesia Paulava <sportgirl16031988@gmail.com>
2023-07-22 13:29:05 +0300

    add modal

--- js/script.js
+++ js/script.js
@@ -26,7 +26,49 @@ const createCard = (item) => {
   return cocktail;
 }
 
+const modalController = ({ modal, btnOpen, time = 300 }) => {
+  const buttonElem = document.querySelector(btnOpen);
+  const modalElem = document.querySelector(modal);
+
+  modalElem.style.cssText = `
+  display: flex;
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity ${time}ms ease-in-out;
+  `;
+
+  const closeModal = (event) => {
+    const target = event.target; //чтобы окно не закрывалось при клике на него
+    const code = event.code; //для закрытия окна по ESC
+
+    if (target === modalElem || code === 'Escape') {
+      modalElem.style.opacity = 0;
+
+      setTimeout(() => { //Для плавности закрытия окна
+        modalElem.style.visibility = 'hidden';
+      }, time);
+
+      window.removeEventListener('keydown', closeModal); //чтобы не писало в консоли нажатые кнопки
+    };
+  };
+
+  const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+    window.addEventListener('keydown', closeModal); //закрытие на кнопку ESC 
+  };
+
+  buttonElem.addEventListener('click', openModal); //открыть модальное окно
+  modalElem.addEventListener('click',closeModal);//закрыть модальное окно
+
+  return { openModal, closeModal};
+}
+
 const init = async () => {
+  modalController({
+    modal: '.modal__order',
+    btnOpen: '.header__btn-order'
+  });
   const goodsListElem = document.querySelector('.goods__list');
   const data = await getData();
 
